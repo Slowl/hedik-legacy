9b7ed2f2053a7b9ee0e3ebbbfb0d38e376bbc7b9
HediK <hedi0475@gmail.com>
2019-01-27 22:49:01 +0100

    made the arrow into a component 'cause i didn't know iw ould reuse it

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import ScrollIntoView from 'react-scroll-into-view'
+
+const ArrowBloc = styled.div`
+  width: 60px;
+  margin : 40px auto;
+  cursor: pointer;
+  opacity: 0;
+  animation : arrow 2.5s ease infinite 1.2s;
+
+  @keyframes arrow {
+    0% {transform: translateY(10px); opacity: 1;}
+    25% {transform: translateY(0px); opacity: 1;}
+    50% {transform: translateY(0px); opacity: 1;}
+    75% {transform: translateY(0px); opacity: 1;}
+    100% {transform: translateY(10px); opacity : 1;}
+  }
+
+  svg {
+    opacity: 0;
+    animation : svg-arrow 2s ease forwards 1.2s;
+
+    @keyframes svg-arrow {
+    100% {opacity: 1;}
+    }
+  }
+
+`
+
+const Arrow = ({ selector }) => {
+
+  return (
+    <ArrowBloc>
+      <ScrollIntoView alignToTop selector={`#${selector}`}>
+        <svg fill="#FFFFFF" height="48" viewBox="0 0 24 24" width="48">
+          <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
+          <path d="M0-.75h24v24H0z" fill="none"/>
+        </svg>
+      </ScrollIntoView>
+    </ArrowBloc>
+  )
+}
+
+export default Arrow
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components'
-import ScrollIntoView from 'react-scroll-into-view'
 import uglyshit from '../assets/img/me.jpg'
 import Parallax from 'parallax-js'
+import Arrow from './Arrow'
 
 
 const IntroContainer = styled.div`
@@ -124,32 +124,6 @@ const Desc = styled.div`
     }
   `
 
-  const Arrow = styled.div`
-    width: 60px;
-    margin : 40px auto;
-    cursor: pointer;
-    opacity: 0;
-    animation : arrow 2.5s ease infinite 1.2s;
-
-    @keyframes arrow {
-      0% {transform: translateY(10px); opacity: 1;}
-      25% {transform: translateY(0px); opacity: 1;}
-      50% {transform: translateY(0px); opacity: 1;}
-      75% {transform: translateY(0px); opacity: 1;}
-      100% {transform: translateY(10px); opacity : 1;}
-    }
-
-    svg {
-      opacity: 0;
-      animation : svg-arrow 2s ease forwards 1.2s;
-
-      @keyframes svg-arrow {
-      100% {opacity: 1;}
-      }
-    }
-
-  `
-
 const Intro = () => {
 
   let target = useRef()
@@ -180,14 +154,7 @@ const Intro = () => {
           <div>Hi, I'm <span>Hedi K.</span></div>
           <div>A <SwitchWordContainer> <span className="weird" data-hover="weird"><span className="passionate">passionate</span></span> </SwitchWordContainer> guy who code the web</div>
 
-          <Arrow>
-            <ScrollIntoView alignToTop selector={"#projects"}>
-              <svg fill="#FFFFFF" height="48" viewBox="0 0 24 24" width="48">
-                <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-                <path d="M0-.75h24v24H0z" fill="none"/>
-              </svg>
-            </ScrollIntoView>
-          </Arrow>
+          <Arrow selector="projects"/>
 
         </Desc>
       </IntroContainer>
