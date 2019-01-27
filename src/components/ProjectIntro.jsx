7b6d658456e4dd6812d91d2cc726442c94604ddc
HediK <hedi0475@gmail.com>
2019-01-27 23:32:15 +0100

    reusable intro for other projects

---
+++ src/components/ProjectIntro.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import Title from './Title'
+import Language from './Language'
+import Description from './Description'
+import Arrow from './Arrow'
+
+const ProjectContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`
+
+const ProjectIntro = ({ title, js, html, css, firebase, react, webpack, children }) => {
+
+  return (
+    <ProjectContainer>
+      <Title title={title} />
+      <Language js={js} html={html} css={css} firebase={firebase} webpack={webpack} react={react} />
+      <Description>
+        {children}
+      </Description>
+      <Arrow selector="showcase" />
+    </ProjectContainer> 
+  )
+}
+
+export default ProjectIntro
