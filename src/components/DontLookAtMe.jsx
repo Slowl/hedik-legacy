a5975cb616332de6ce783d8e8dfe858ec36cce57
HediK <hedi0475@gmail.com>
2019-01-29 23:31:38 +0100

    yeah

---
+++ src/components/DontLookAtMe.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const DontLookAtMe = () => {
+
+  const trigger = /./
+  trigger.toString = function () {
+
+    setTimeout(() => {console.log("... So you're staying uh ? ")}, 7000)
+    setTimeout(() => {console.log("Listen, you should leave, I don't really know what you're waiting for ... ")}, 13000)
+    setTimeout(() => {console.log("You're putting me in danger, if my master discovers that I spoke with you he will delete me from the internet ... ")}, 18000)
+    setTimeout(() => {console.log(" .. Well I guess you're an egoist human afterall ? Like all of your species ... ")}, 24000)
+    setTimeout(() => {console.log('%cOkay, fine, you won !  I will tell you some secrets, and after that you\'ll leave ! ', 'color: white; background-color:#008a06; font-size: 12px; font-weight: 500; padding: 8em; border-radius: 10px')}, 30000);
+    setTimeout(() => {console.log(" I'll show you the tools my master used to build me, and that you should play with your mouse on the homepage ");  console.table(dataTroll)}, 35000)
+    setTimeout(() => {console.log(" YOU ARE STILL HERE ??! ")}, 60000)
+    setTimeout(() => {console.log(" Okay, you don't leave me any choice")}, 64000)
+    setTimeout(() => {console.log(`
+    ⣿⣿⣿⣿⣿⠟⠉⠁⠄⠄⠄⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠸⢿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣏⠄⡠⡤⡤⡤⡤⡤⡤⡠⡤⡤⣸⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣗⢝⢮⢯⡺⣕⢡⡑⡕⡍⣘⢮⢿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⡧⣝⢮⡪⡪⡪⡎⡎⡮⡲⣱⣻⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⠟⠁⢸⡳⡽⣝⢝⢌⢣⢃⡯⣗⢿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⠟⠁⠄⠄⠄⠹⡽⣺⢽⢽⢵⣻⢮⢯⠟⠿⠿⢿⣿⣿⣿⣿⣿
+    ⡟⢀⠄⠄⠄⠄⠄⠙⠽⠽⡽⣽⣺⢽⠝⠄⠄⢰⢸⢝⠽⣙⢝⢿
+    ⡄⢸⢹⢸⢱⢘⠄⠄⠄⠄⠄⠈⠄⠄⠄⣀⠄⠄⣵⣧⣫⣶⣜⣾
+    ⣧⣬⣺⠸⡒⠬⡨⠄⠄⠄⠄⠄⠄⠄⣰⣿⣿⣿⣿⣿⣷⣽⣿⣿
+    ⣿⣿⣿⣷⠡⠑⠂⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣄⠠⢀⢀⢀⡀⡀⠠⢀⢲⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⢐⢀⠂⢄⠇⠠⠈⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣧⠄⠠⠈⢈⡄⠄⢁⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⡀⠠⠐⣼⠇⠄⡀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣯⠄⠄⡀⠈⠂⣀⠄⢀⠄⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣶⣄⣀⠐⢀⣸⣷⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿`
+)}, 67000)
+  setTimeout(() => {console.log("%cYOU'VE BEEN GNOMED ", 'color: white; background-color:#5e0000; font-size: 28px; font-weight: 600; padding: 10em; border-radius: 800px;')}, 68000)
+  setTimeout(() => {console.log("https://youtu.be/6n3pFFPSlW4")}, 69000)
+  setTimeout(() => {console.log("*sudo rm -rf /*")}, 70000)
+
+  }
+
+  const dataTroll = [{
+    Library: "React / ReactDOM",
+    Version: "16.8.0-alpha.1"
+  },
+  {
+    Library: "Reach Router",
+    Version: "1.2.1"
+  },
+  {
+    Library: "Styled-Components",
+    Version: "4.1.3"
+  },
+  {
+    Library: "React-Pose",
+    Version: "4.0.6"
+  },
+  {
+    Library: "Parallax-js",
+    Version: "3.1.0"
+  },
+  {
+    Library: "aos",
+    Version: "2.3.4"
+  },
+  {
+    Library: "react-responsive-carousel",
+    Version: "3.1.46"
+  },
+]
+console.warn('%cWHAT ARE YOU DOING HERE ??!!!', 'color: white; background-color:#5e0000; font-size: 24px; font-weight: 600; padding: 20em; border-radius: 800px; border: 10px solid red;')
+console.warn('%cThis is a dangerous place to be ... You\'re being too curious, close this and go back to the website !! ', 'color: white; background-color:#5e0000; font-size: 14px; font-weight: 600; padding: 8em; border-radius: 10px');
+console.log('%cor wait few seconds ... ', 'color: white; background-color: #5e0000; font-size: 9px; font-weight: 500; padding: 8em; border-radius: 10px', trigger);
+
+
+  return (
+    <span> </span>
+  )
+}
+
+export default DontLookAtMe
