7adba9c3206ac28f04eb4bf07b775adb4cf5bd81
HediK <hedi0475@gmail.com>
2019-02-03 16:19:15 +0100

    added Linkfluence page complete

---
+++ src/pages/Linkfluence.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useEffect } from 'react';
+import ProjectIntro from '../components/ProjectIntro'
+import Showcase from '../components/Showcase'
+import ProjectNav from '../components/ProjectNav'
+
+const LkfImg = [
+    { src: "https://i.ibb.co/qjNdH4n/list-components-1.png", legend:"A component that display a preview of a specific component and the code attached to it", alt:"homepage", importance:"high" },
+    { src: "https://i.ibb.co/2hb7RqC/list-components-2.png", legend:"The code attached to a component displayed", alt:"gallery", importance:"low" },
+    { src: "https://i.ibb.co/k3bZVtx/playground-1.png", legend:"This is a Playground component to allow the user to play with the props of a component", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/ZLDKVRQ/playground-2.png", legend:"Live preview of the Playground, modifying the component and the code attached to it", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/vYDmN45/toolbox.png", legend:"Toolbox allowing to change some properties when we don't use the Playground component", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/z4VGM4R/props-Table.png", legend:"PropsTable component to display the declared propsTypes of a component", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/bRMt4Rj/documentation-1.png", legend:"Example of a documentaion page created with multiple custom components (Titles, Section togglable ...)", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/P5PQxJN/documentation-2.png", legend:"Example of a documentaion page (2)", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/3C39YZb/before-black-theme.png", legend:"Example of the white theme", alt:"contact", importance:"low" },
+    { src: "https://i.ibb.co/LprH0Cz/after-black-theme.png", legend:"Example of the dark theme", alt:"contact", importance:"low" },
+  ]
+
+const Linkfluence = () => {
+
+  useEffect( () => {
+    window.setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 400);
+
+    document.title = " ~ hedik/linkfluence"
+    return () => document.title = " ~ hedik/"
+
+  })
+
+  return (
+    <div>
+      <ProjectIntro title="Linkfluence Styleguide" react html css webpack>
+        Storybook ? Styleguidist ? Docz ? Docusaurus ?  ... All of these tools in one ? Well that's more or less what I did for Linkfluence.
+        <div>
+          Linkfluence's web engineers needed a tool to document their reacts components, write documentation, preview components
+          and be able to show them to non-web-enginners people (like designers for example).
+          <div>
+            Existing solutions were not matching all their needs so I built a homemade solution with all the features from these different tools in one place !
+          </div>
+        </div>
+        <div>
+          In a few words : it's a library of components allowing web enginners to easily build a complete styleguide.
+        </div>
+      </ProjectIntro>
+      <Showcase dataImg={LkfImg} />
+      <ProjectNav noLive />
+    </div>
+  )
+}
+
+export default Linkfluence
