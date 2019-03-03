dfc01b68cdf8193b6562017293e9620beda9c8d9
HediK <hedi0475@gmail.com>
2019-03-03 14:40:05 +0100

    modified linkfluence description

--- src/pages/Linkfluence.jsx
+++ src/pages/Linkfluence.jsx
@@ -35,10 +35,10 @@ const Linkfluence = () => {
       <ProjectIntro title="Linkfluence Styleguide" react js html css webpack>
         Storybook ? Styleguidist ? Docz ? Docusaurus ?  ... All of these tools in one ? Well that's more or less what I did for Linkfluence.
         <div>
-          Linkfluence's web engineers needed a tool to document their reacts components, write documentation, preview components
+          Linkfluence's web engineers needed a tool to document their React components, write documentation, preview components
           and be able to show them to non-web-enginners people (like designers for example).
           <div>
-            Existing solutions were not matching all their needs so I built a homemade solution with all the features from these different tools in one place !
+            Existing solutions were not matching all their needs so I built a homemade solution from scratch, with all the features of these different tools in one place !
           </div>
         </div>
         <div>
